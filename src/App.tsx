aedd9c0fedab5197f972048f41a48fb9b6f4033b
griminir <viktor.elliot.king@gmail.com>
2024-09-13 10:10:15 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <input type='text' className='form-control' />
+      </div>
+    </>
+  );
+}
+
+export default App;
