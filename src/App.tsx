c983b62da3048fc49d82f43c23177632364d084d
griminir <viktor.elliot.king@gmail.com>
2024-09-16 12:24:49 +0200

    added setup for a delete call to an api, except the api im testing it on does not have a delete, so its simulated

--- src/App.tsx
+++ src/App.tsx
@@ -12,21 +12,28 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
     // const fetchUsers = async () => {
     // try {
     // const res = await
+    setLoading(true);
     axios
       .get<User[]>('https://jsonplaceholder.typicode.com/users', {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false); // duplication to make it work in strict mode
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false); // duplication to make it work in strict mode
       });
+    // .finally(() => setLoading(false)); // how its suppose to be done (does not work in strict mode)
     //   setUsers(res.data);
     // } catch (error) {
     //   setError((error as AxiosError).message);
@@ -36,13 +43,34 @@ function App() {
     return () => controller.abort();
   }, []);
 
+  const deleteUser = (user: User) => {
+    const original = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+    axios
+      .delete('https://jsonplaceholder.typicode.com/users/' + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(original);
+      });
+  };
+
   return (
     <>
       {error && <p className='text-danger'>{error}</p>}
-      <ul>
+      {isLoading && <div className='spinner-border'></div>}
+      <ul className='list-group'>
         {users.map((user) => (
-          <li key={user.id}>
+          <li
+            key={user.id}
+            className='list-group-item d-flex justify-content-between'
+          >
             {user.id}: {user.name}
+            <button
+              className='btn btn-outline-danger'
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
           </li>
         ))}
       </ul>
